93e76faf7dc65261f35b24cd170dbdcd1cfd3d5e
Tom McLaughlin <tom@codedown.io>
2023-04-06 17:21:25 -0700

    Don't re-download existing versions

--- src/RegistryToNix/Process.hs
+++ src/RegistryToNix/Process.hs
@@ -28,14 +28,17 @@ processPackage (Package {packageVersions=(Versions versions), ..}) = do
   debug [i|Processing #{packageName} (#{repo})|]
 
   versions' <- (M.fromList <$>) $ forM (M.toList versions) $ \(k, version@(Version {..})) -> do
-    debug [i|Fetching version #{k}|]
-    (exitCode, sout, serr) <- liftIO $ readCreateProcessWithExitCode ((proc "nix-prefetch-git" [T.unpack repo, "--rev", T.unpack gitTreeSha1]) {
-                                                                         env = Just [("GIT_TERMINAL_PROMPT", "0")]
-                                                                         }) ""
-    case exitCode of
-      ExitSuccess -> case A.eitherDecode $ BL.pack sout of
-        Left err -> expectationFailure [i|Failed to decode nix-prefetch-git output: #{err}. Stdout: #{sout}. Stderr: #{serr}|]
-        Right (NixPrefetchGit {..}) -> return (k, version { nixSha256 = Just sha256 })
-      ExitFailure n -> expectationFailure [i|Failed to nix-prefetch-git #{repo} --rev #{gitTreeSha1} (exited with #{n}). Stdout: #{sout}. Stderr: #{serr}|]
+    case nixSha256 of
+      Just _ -> return (k, version) -- No need to re-fetch
+      Nothing -> do
+        debug [i|Fetching version #{k}|]
+        (exitCode, sout, serr) <- liftIO $ readCreateProcessWithExitCode ((proc "nix-prefetch-git" [T.unpack repo, "--rev", T.unpack gitTreeSha1]) {
+                                                                             env = Just [("GIT_TERMINAL_PROMPT", "0")]
+                                                                             }) ""
+        case exitCode of
+          ExitSuccess -> case A.eitherDecode $ BL.pack sout of
+            Left err -> expectationFailure [i|Failed to decode nix-prefetch-git output: #{err}. Stdout: #{sout}. Stderr: #{serr}|]
+            Right (NixPrefetchGit {..}) -> return (k, version { nixSha256 = Just sha256 })
+          ExitFailure n -> expectationFailure [i|Failed to nix-prefetch-git #{repo} --rev #{gitTreeSha1} (exited with #{n}). Stdout: #{sout}. Stderr: #{serr}|]
 
   liftIO $ writeVersionsToml (T.unpack packageFullPath </> "Versions.toml") (Versions versions')
