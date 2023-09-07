81a79057d16107861161fe05cb7e04808c685e2a
thomasjm <tom@codedown.io>
2023-09-06 23:51:54 -0700

    Bump to GHC 9.2.8

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        default = pkgs.haskell.packages.ghc927.callPackage ./default.nix {};
+        default = pkgs.haskell.packages.ghc928.callPackage ./default.nix {};
       in
         rec {
           packages = rec {
